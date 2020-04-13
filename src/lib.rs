e0485e8c59060dccd6ddc850f97169c2522f000e
bartekus <bartekus@gmail.com>
2020-04-12 18:45:35 -0600

    Step 7 - Keep it simple and test it

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,17 @@ pub fn print(limit: u8) {
 }
 
 fn generate_sequence(limit: u8) -> Vec<u8> {
-    let mut numbers = Vec::new();
-    for n in 1..=limit {
-        numbers.push(n);
-    }
-    numbers
+    (1..=limit).collect()
 }
 
 fn output_sequence(numbers: &[u8]) {
     for n in numbers {
         println!("{}", n);
     }
+}
+
+#[test]
+fn generate_sequence_should_work() {
+    let result = generate_sequence(3);
+    assert_eq!(result, &[1, 2, 3]);
 }
