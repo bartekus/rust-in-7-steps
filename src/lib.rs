bdbc82fd43fd53e46bec746c0e983fe222e3d5a8
bartekus <bartekus@gmail.com>
2020-04-12 18:32:50 -0600

    Step 3 - Using std::vec::Vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub fn print() {
-    let numbers = [1, 2, 3, 4, 5];
-    // let () = numbers;
-    for n in numbers.iter() {
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    for n in numbers {
         println!("{}", n);
     }
 }
