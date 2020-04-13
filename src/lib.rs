547465760ced4cea729216587a1570ad10724e9b
bartekus <bartekus@gmail.com>
2020-04-12 18:38:12 -0600

    Step 4 - Intro into ownership

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 pub fn print() {
     let numbers = vec![1, 2, 3, 4, 5];
 
+    output_sequence(numbers);
+    output_sequence(numbers); // <- Will error here
+}
+
+fn output_sequence(numbers:Vec<u8>) {
     for n in numbers {
         println!("{}", n);
     }
